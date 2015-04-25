57c125596a09bd3fc9861b0b6cb3efca28147466
Peter Lejeck <peter.lejeck@gmail.com>
2015-04-25 14:39:12 -0700

    Show characters on manga pages

--- frontend/app/controllers/manga/index.js
+++ frontend/app/controllers/manga/index.js
@@ -2,7 +2,29 @@ import Ember from 'ember';
 import HasCurrentUser from '../../mixins/has-current-user';
 
 export default Ember.ObjectController.extend(HasCurrentUser, {
+  showFullCharacters: false,
+  fullCharacters: null,
+
+  displayCast: function() {
+    if (this.get('showFullCharacters') && this.get('fullCharacters')) {
+      return this.get('fullCharacters');
+    } else {
+      return this.get('model.featuredCastings');
+    }
+  }.property('model.featuredCastings', 'fullCharacters', 'showFullCharacters'),
+
   mangaLibraryEntryExists: function() {
     return (!Ember.isNone(this.get('model.mangaLibraryEntry'))) && (!this.get('model.mangaLibraryEntry.isDeleted'));
-  }.property('model.mangaLibraryEntry', 'model.mangaLibraryEntry.isDeleted')
+  }.property('model.mangaLibraryEntry', 'model.mangaLibraryEntry.isDeleted'),
+
+  actions: {
+    showMoreCharacters: function() {
+      this.set('showFullCharacters', true);
+      this.send('loadFullCharacters');
+    },
+
+    showFewerCharacters: function() {
+      this.set('showFullCharacters', false);
+    }
+  }
 });
